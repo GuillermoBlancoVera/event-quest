9210807ac6e2b0073e9f76d06d7bce4939e7aa82
GuillermoBlancoVera <guillermoblancovera@gmail.com>
2026-08-22 15:42:26 +0200

    fix: allow production pages origin for media

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -21,7 +21,7 @@ variable "custom_domain" {
 variable "web_origins" {
   type        = list(string)
   description = "Allowed browser origins for the API and direct S3 media uploads."
-  default     = ["https://guillermowhite.github.io", "http://localhost:5173"]
+  default     = ["https://guillermoblancovera.github.io", "http://localhost:5173"]
 }
 
 variable "lambda_artifact_bucket" {
